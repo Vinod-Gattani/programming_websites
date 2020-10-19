ba0c4eaf6921c7174203d5d4c9e4ba27ada66a2f
Vinod Gattani <vinodgattani1990@gmail.com>
2020-10-19 22:27:56 +0530

    1007 solution updated

--- leetcode/1007_minDominoRotations.py
+++ leetcode/1007_minDominoRotations.py
@@ -4,34 +4,29 @@ from typing import List
 class Solution:
     def minDominoRotations(self, A: List[int], B: List[int]) -> int:
         
-        A_count = defaultdict(lambda : 0)
-        B_count = defaultdict(lambda : 0)
-        total_count = defaultdict(lambda : 0)
-        same_count = defaultdict(lambda : 0)
+        len_A = len(A)
+        len_B = len(B)
         
-        for a, b in zip(A, B):
-            A_count[a] += 1
-            B_count[b] += 1
-            total_count[a] += 1
-            if b == a:
-                same_count[a] += 1
-            else:
-                total_count[b] += 1
-            
-        max_len = len(A)
-        result = len(A)
-        
-        for key, value in total_count.items():
-            if value == max_len:
-                result = min(result,
-                             A_count[key]-same_count[key],
-                             B_count[key]-same_count[key]
-                            )
-                
-        if result == len(A):
+        if len_A != len_B:
             return -1
-        else:
-            return result
+        result = float('inf')
+        
+        for i in [A[0],B[0]]:
+            all_same = True
+            count_A = 0
+            count_B = 0
+            for a, b in zip(A, B):
+                
+            
+                if a != i and b != i:
+                    break
+
+                count_A += a==i
+                count_B += b==i
+            else:
+                result = min(result, len_A-count_A, len_A-count_B)
+        
+        return -1 if result == float('inf') else result
 
 if __name__ == "__main__":
     A = [2,1,2,4,2,2]
